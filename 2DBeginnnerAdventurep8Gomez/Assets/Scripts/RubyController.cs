3bec909dae4b7664ab9aef0e7384f260882461a4
stanley091610 <754083@student.dusd.net>
2024-10-19 13:20:45 -0700

    today i was writing the scripting so ruby can move to the sides.

--- 2DBeginnnerAdventurep8Gomez/Assets/Scripts/RubyController.cs
+++ 2DBeginnnerAdventurep8Gomez/Assets/Scripts/RubyController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class RubyContro : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertital = Input.GetAxis("vertital");
+
+
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertital;
+
+        transform.position = position;
     }
 }
