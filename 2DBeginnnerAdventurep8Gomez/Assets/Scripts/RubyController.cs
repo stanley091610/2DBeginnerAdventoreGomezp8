e92816d6b328024e08b127beab4021464d2b77f7
stanley091610 <754083@student.dusd.net>
2024-10-21 10:24:29 -0700

    today I'm  my ground so ruby can walk in the floor .

--- 2DBeginnnerAdventurep8Gomez/Assets/Scripts/RubyController.cs
+++ 2DBeginnnerAdventurep8Gomez/Assets/Scripts/RubyController.cs
@@ -7,19 +7,20 @@ public class RubyContro : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;   
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertital = Input.GetAxis("vertital");
+        float vertical = Input.GetAxis("Vertical");
 
 
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertital;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         transform.position = position;
     }
