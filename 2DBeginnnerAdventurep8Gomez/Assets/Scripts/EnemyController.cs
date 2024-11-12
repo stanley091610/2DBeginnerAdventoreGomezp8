709389cc6d1b47890fa1d26ff113f1c20ea71376
stanley091610 <754083@student.dusd.net>
2024-11-12 15:30:51 -0800

    today i finish writing my enemy scrips and add animation.

--- 2DBeginnnerAdventurep8Gomez/Assets/Scripts/EnemyController.cs
+++ 2DBeginnnerAdventurep8Gomez/Assets/Scripts/EnemyController.cs
@@ -4,27 +4,54 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 3.0f;
+    public bool vertical;
+    public float changeTime = 3.0f;
 
     Rigidbody2D rigidbody2d;
 
+    float timer;
+    int direction = 1;
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        } 
     }
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + Time.deltaTime * speed;
+        if(vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed * direction;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+        }
 
-        rigidbody2d.MovePosition(position);
+        
+        rigidbody2d.MovePosition(position);  
     }
-}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+} 
