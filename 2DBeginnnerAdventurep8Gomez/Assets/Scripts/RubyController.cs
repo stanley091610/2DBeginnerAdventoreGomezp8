249d74da48aff39ebfb94323755411f89715d136
stanley091610 <754083@student.dusd.net>
2024-11-14 15:34:36 -0800

    today I trying to finish spirit animation for the enemy.

--- 2DBeginnnerAdventurep8Gomez/Assets/Scripts/RubyController.cs
+++ 2DBeginnnerAdventurep8Gomez/Assets/Scripts/RubyController.cs
@@ -18,11 +18,15 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
 
    
     }
@@ -33,6 +37,12 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+        }
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
