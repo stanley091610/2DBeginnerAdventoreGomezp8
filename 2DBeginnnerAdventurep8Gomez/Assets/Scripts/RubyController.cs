e33179b28530ab345525efa16fb525d76f2dbbd2
stanley091610 <754083@student.dusd.net>
2024-11-22 23:22:21 -0800

    today I finish with my project of 2DBeginnerAdventure because today was the las day for this project.

--- 2DBeginnnerAdventurep8Gomez/Assets/Scripts/RubyController.cs
+++ 2DBeginnnerAdventurep8Gomez/Assets/Scripts/RubyController.cs
@@ -24,6 +24,10 @@ public class RubyController : MonoBehaviour
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
 
+    AudioSource audioSource;
+    public AudioClip throwSound;
+    public AudioClip hitSound;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,7 +35,8 @@ public class RubyController : MonoBehaviour
         currentHealth = maxHealth;
         animator = GetComponent<Animator>();
 
-   
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -63,6 +68,19 @@ public class RubyController : MonoBehaviour
         {
             Launch();
         }
+
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if(hit.collider != null)
+            {
+               NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if(character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
     
     }
     void FixedUpdate()
@@ -85,11 +103,12 @@ public class RubyController : MonoBehaviour
             }
             isInvincible = true;
             invincibleTimer = timeInvincible;
+            PlaySound(hitSound);
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);   
+        UIHealthBar.instance.SetValue(currentHealth/(float)maxHealth);   
     }
-
+   
     void Launch()
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
@@ -98,5 +117,11 @@ public class RubyController : MonoBehaviour
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
+        PlaySound(throwSound);
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
